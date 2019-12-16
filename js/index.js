dc1599630a13b2dd3adf91848b5cba131fdaa8d6
Skye Geraghty <skyetay03@googlemail.com>
2019-12-16 10:43:18 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -16,3 +16,13 @@ input.addEventListener('keydown', function(e) {
   }
 });
 
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
